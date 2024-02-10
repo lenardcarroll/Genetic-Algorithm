9c491efe2443afecfe228c827cb992b6a85c0e85
Lenard Carroll <86952266+lenardcarroll@users.noreply.github.com>
2024-02-10 19:09:54 +0200

    Update GA.py
    Fixed issue with VERBOSE

--- GA.py
+++ GA.py
@@ -28,7 +28,7 @@ class geneticAlgorithm:
         POPULATION_SIZE = int(POPULATION_SIZE)
         NUMBER_OF_GENERATIONS = int(NUMBER_OF_GENERATIONS)
 
-        if MUTATION_PERCENTAGE < 0 or CROSSOVER_RATE < 0 or ELITISM_RATE < 0 or (VERBOSE !=0 or VERBOSE != 1) or POPULATION_SIZE == 0 or NUMBER_OF_GENERATIONS == 0:
+        if MUTATION_PERCENTAGE < 0 or CROSSOVER_RATE < 0 or ELITISM_RATE < 0 or (VERBOSE !=0 and VERBOSE != 1) or POPULATION_SIZE == 0 or NUMBER_OF_GENERATIONS == 0:
             print("Your arguments are incorrect! Please fix it")
             exit()
 
